278d990b90f9ad0acf56065de28f4b15417fbee2
Rickson Oliveira <88171582+Rickson298@users.noreply.github.com>
2024-07-28 21:51:30 -0300

    `chore: Disable code splitting in tsup configuration`

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
   entry: ['src'],
-  format: ['cjs', 'esm'], // Build for commonJS and ESmodules
+  format: ['esm'], // Build for commonJS and ESmodules
   dts: true, // Generate declaration file (.d.ts)
-  splitting: true,
+  splitting: false,
   sourcemap: false,
   clean: true,
 });
