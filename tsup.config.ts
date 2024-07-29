72db636fce0830101551f6076719780fb53920e2
Rickson Oliveira <88171582+Rickson298@users.noreply.github.com>
2024-07-28 21:43:13 -0300

    `chore: Update tsup configuration to build for commonJS and ESmodules and enable code splitting`

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
   entry: ['src'],
-  format: ['cjs'], // Build for commonJS and ESmodules
+  format: ['cjs', 'esm'], // Build for commonJS and ESmodules
   dts: true, // Generate declaration file (.d.ts)
   splitting: true,
   sourcemap: false,
